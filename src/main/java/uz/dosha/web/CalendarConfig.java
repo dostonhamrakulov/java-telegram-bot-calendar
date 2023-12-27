ffc8b48e29f842c4e007198107a1533deeb15782
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2023-12-27 23:26:17 +0100

    creating calendar generator - first version of calendar

--- src/main/java/uz/dosha/web/CalendarConfig.java
+++
@@ -1,3 +0,0 @@
-package uz.dosha.web;
-
-public class CalendarConfig { }
---
+++ src/main/java/uz/dosha/web/CalendarGenerator.java
@@ -0,0 +1,79 @@
+package uz.dosha.web;
+
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CalendarGenerator {
+    public static InlineKeyboardMarkup buildCalendar() {
+        final InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        final List<List<InlineKeyboardButton>> rows = new ArrayList<>();
+
+        List<InlineKeyboardButton> inlineKeyboardButtons = new ArrayList<>();
+
+        int daysOfCurrentMonth = YearMonth.now().lengthOfMonth();
+        String[] weekDays = new String[]{"D", "S", "CH", "P", "J", "SH", "Y"};
+
+        for (String weekDay: weekDays) {
+            final InlineKeyboardButton in = new InlineKeyboardButton();
+            in.setText(weekDay);
+            in.setCallbackData(weekDay);
+            inlineKeyboardButtons.add(in);
+        }
+
+        rows.add(inlineKeyboardButtons);
+
+        inlineKeyboardButtons = new ArrayList<>();
+
+        LocalDate today = LocalDate.now();
+        int weekDaysCounter = LocalDate.of(today.getYear(), today.getMonth(), 1).getDayOfWeek().getValue() - 1;
+
+        // adding empty buttons
+        for (int i = 0; i < weekDaysCounter; i++) {
+            final InlineKeyboardButton in = new InlineKeyboardButton();
+            in.setText(" ");
+            in.setCallbackData(" ");
+            inlineKeyboardButtons.add(in);
+        }
+
+        for (int i = 1; i <= daysOfCurrentMonth; i++) {
+            final InlineKeyboardButton in = new InlineKeyboardButton();
+            in.setText("" + i);
+            in.setCallbackData("" + i);
+            inlineKeyboardButtons.add(in);
+            weekDaysCounter += 1;
+
+            if (weekDaysCounter == 7) {
+                rows.add(inlineKeyboardButtons);
+
+                inlineKeyboardButtons = new ArrayList<>();
+                weekDaysCounter = 0;
+            }
+        }
+
+        inlineKeyboardButtons = new ArrayList<>();
+
+        InlineKeyboardButton in = new InlineKeyboardButton();
+        in.setText("<<");
+        in.setCallbackData("<<");
+        inlineKeyboardButtons.add(in);
+
+        in = new InlineKeyboardButton();
+        in.setText(today.getMonth().name().substring(0, 3) + ", " + today.getYear());
+        in.setCallbackData(today.getMonth().name());
+        inlineKeyboardButtons.add(in);
+
+        in = new InlineKeyboardButton();
+        in.setText(">>");
+        in.setCallbackData(">>");
+        inlineKeyboardButtons.add(in);
+        rows.add(inlineKeyboardButtons);
+        inlineKeyboardMarkup.setKeyboard(rows);
+
+        return inlineKeyboardMarkup;
+    }
+}
