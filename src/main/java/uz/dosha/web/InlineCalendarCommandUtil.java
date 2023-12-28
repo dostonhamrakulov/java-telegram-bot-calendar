e46a01c18ecb923266a7c84bfc5d6719349f9a46
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2023-12-28 15:12:44 +0100

    Extending InlineCalendarCommandUtil to handle ignore and navigation buttons

--- src/main/java/uz/dosha/web/InlineCalendarCommandUtil.java
+++ src/main/java/uz/dosha/web/InlineCalendarCommandUtil.java
@@ -8,6 +8,8 @@ public class InlineCalendarCommandUtil {
 
     public static final String CALENDAR_COMMAND_PREFIX = "CAL_CM:";
     public static final String CALENDAR_COMMAND_DATE = "DATE:";
+    public static final String CALENDAR_COMMAND_IGNORE = "IGNORE";
+    public static final String CALENDAR_COMMAND_NAVIGATION = "NAVIGATE:";
 
     public static boolean isInlineCalendarClicked(final Update update) {
         return update.getCallbackQuery() != null
@@ -21,7 +23,27 @@ public class InlineCalendarCommandUtil {
                && update.getCallbackQuery().getData().startsWith(CALENDAR_COMMAND_PREFIX + CALENDAR_COMMAND_DATE);
     }
 
+    public static boolean isCalendarIgnoreButtonClicked(final Update update) {
+        return update.getCallbackQuery() != null
+               && update.getCallbackQuery().getData() != null
+               && update.getCallbackQuery().getData().startsWith(CALENDAR_COMMAND_PREFIX + CALENDAR_COMMAND_IGNORE);
+    }
+
+    public static boolean isCalendarNavigationButtonClicked(final Update update) {
+        return update.getCallbackQuery() != null
+               && update.getCallbackQuery().getData() != null
+               && update.getCallbackQuery().getData().startsWith(CALENDAR_COMMAND_PREFIX + CALENDAR_COMMAND_NAVIGATION);
+    }
+
     public static LocalDate extractDate(final Update update) {
         return DateTimeUtil.convertToDate(update.getCallbackQuery().getData().replace(CALENDAR_COMMAND_PREFIX + CALENDAR_COMMAND_DATE, ""));
+    }
+
+    public static LocalDate extractNavigationDate(final Update update) {
+        if (!isCalendarNavigationButtonClicked(update)) {
+            return null;
+        }
+
+        return DateTimeUtil.convertToDate(update.getCallbackQuery().getData().replace(CALENDAR_COMMAND_PREFIX + CALENDAR_COMMAND_NAVIGATION, ""));
     }
 }
